5f18343e851565505f1a06620ea4ff667072d4e8
Livid Rose <andredarksoul40@gmail.com>
2026-03-12 02:54:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-800591507-db7c9",
+  "appId": "1:879912626424:web:66e5bf816ccc97f5bc730d",
+  "apiKey": "AIzaSyBrVEj_-NFnL_UlH61im9b1IJz2mP_oziE",
+  "authDomain": "studio-800591507-db7c9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "879912626424"
+};
